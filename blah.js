1fcd9a200e071a8159877f01241443983643dd32
blahhhhhhblah <jbw.newton@gmail.com>
2025-07-28 13:29:50 +0100

    Update blah.js

--- blah.js
+++ blah.js
@@ -3,6 +3,16 @@
 let firstname;
 document.getElementById("button1").onclick = function(){
     firstname = document.getElementById("input1").value;
+    if (firstname == "Maya"){
+        document.getElementById("love").textContent = "I truly love " + firstname ;
+
+    }
+    else if (firstname == "maya"){
+         document.getElementById("love").textContent = "I truly love " + firstname ;
+    }
+    else{
+        document.getElementById("love").textContent = "Incorrect name entered";
+    }
     document.getElementById("input1").value = "";
-    document.getElementById("love").textContent = "I truly love " + firstname ;
+   
 }
